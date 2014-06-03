611ae33fe6d168320a22cfed0cfddb185bbee528
justine jones <justinejones@justines-MacBook-Air.local>
2014-06-03 13:31:28 -0400

    add ex 1 and 2

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+
+# this is calculates a good tip
+meal = 55
+tip = .15 * meal
+
+puts "a good tip for a #{meal} dollar meal is #{tip}"
+
+# this adds a string and an integer converted to a string together
+puts "I am cold" + 5.to_s
+
+
+#this multiplies 2 numbers together via interpolation
+num1 = 45628 
+num2 = 7839
+
+puts " the value of #{num1} multiplied by #{num1} is #{num1*num2}"
+
+# this experssion will evaluate to true
+exp= (true&&false)|| (false&&true)|| !(false&&false)
